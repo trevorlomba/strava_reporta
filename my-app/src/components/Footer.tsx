53e12e64aa7f7ee82a83be9abf69faf007e34309
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2023-04-14 21:17:41 -0400

    report slider and bug fixes

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div className="footer">
-        <p>Development and analysis by Trevor "TJ" Lomba. <a href="https://www.linkedin.com/in/trevorlomba/">Let's get in touch!</a></p>
+        <p>Development and analysis by Trevor Lomba. <a href="https://www.linkedin.com/in/trevorlomba/">Let's get in touch!</a></p>
       </div>
     </div>
   );
